84c998ca048ff30cee976c1f4d36e531d30372ee
Mark Music <markmusic999@gmail.com>
2023-04-23 23:27:57 -0600

    ft[storyBanner] => 💎 Update story colro

--- src/components/StoryBanner.tsx
+++ src/components/StoryBanner.tsx
@@ -14,16 +14,16 @@ const StoryBanner = () => {
             injustice
           </p>
           <button
-            className=" w-[125px] flex-row space-x-[4px] rounded-[14px] bg-[#EA336C26] px-[14px] py-[8px] transition-all duration-300 hover:scale-[1.05]  hover:bg-[#C127BF40] sm:flex md:hidden"
+            className=" w-[125px] flex-row space-x-[4px] rounded-[14px] bg-[#EB505626] px-[14px] py-[8px] transition-all duration-300 hover:scale-[1.05]  hover:bg-[#C127BF40] sm:flex md:hidden"
             onClick={() => {
               window.open(data.story, "_blank");
             }}
           >
-            <p className="pr-[3px] font-display text-[0.9rem] font-[600] text-[#EA336C]">
+            <p className="pr-[3px] font-display text-[0.9rem] font-[600] text-[#EB5056]">
               View Story
             </p>
             <div className="mt-[2px] sm:scale-[1.3] lg:scale-[1]">
-              <ArrowIcon color="#EA336C" />
+              <ArrowIcon color="#EB5056" />
             </div>
           </button>
         </div>
